04b6f04e7ffbdd829cd3b491116e7b333598e7bd
WeiChengTW <chang93928@gmail.com>
2025-06-01 23:03:20 +0800

    cpath

--- PTT-Crawler-master/Crawler.py
+++ PTT-Crawler-master/Crawler.py
@@ -179,7 +179,7 @@ class PttCrawler:
 
         try:
             with open(
-                f"PTT-Crawler-master\data\{filename}.json",
+                f"{filename}.json",
                 "wb+",
             ) as op:
                 op.write(json.dumps(data, indent=4, ensure_ascii=False).encode("utf-8"))
